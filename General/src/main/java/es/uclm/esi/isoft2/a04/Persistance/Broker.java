4822e9141aa7871a2c22d5fdee7aa6abb997d00a
Guadalupe-Plaza490 <Guadalupe-Plaza@alu.uclm.es>
2020-12-12 13:02:18 +0100

    Implementation of the Broker

--- General/src/main/java/es/uclm/esi/isoft2/a04/Persistance/Broker.java
+++ General/src/main/java/es/uclm/esi/isoft2/a04/Persistance/Broker.java
@@ -1,38 +1,93 @@
 package es.uclm.esi.isoft2.a04.Persistance;
 
-import java.util.ArrayList;
+import java.sql.*;
+import java.util.Vector;
 
 public class Broker {
-
+	
+	final static String DBUSER = "A04dbservice";
+	final static String DBPASS = "@ISoft2.2020#";
+	
+	private static final String unformatedString = "jdbc:mysql://172.20.48.70:3306/"+DBUSER+"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	protected static Connection connection;
 	protected static Broker instance;
-	private String unformatedString;
-
-	protected Broker Broker() {
-		// TODO - implement Broker.Broker
-		throw new UnsupportedOperationException();
+	
+	
+	/**
+	 * 
+	 * Description: Constructor method.
+	 * 
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws ClassNotFoundException
+	 */
+	protected Broker() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+		
 	}
-
-	public static Broker getBroker() {
-		// TODO - implement Broker.getBroker
-		throw new UnsupportedOperationException();
+	/**
+	 * 
+	 * @return An instance of the Broker
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws ClassNotFoundException
+	 */
+	public static Broker getBroker() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		
+		if (instance == null) {
+			instance = new Broker();
+		}
+		return instance;
 	}
 
 	/**
 	 * 
 	 * @param sql
+	 * @throws SQLException 
 	 */
-	public ArrayList<ArrayList<Object>> read(String sql) {
-		// TODO - implement Broker.read
-		throw new UnsupportedOperationException();
+	public Vector<Vector<Object>> read(String sql) throws SQLException {
+		
+		Statement statement;
+		ResultSet result_set;
+		Vector<Object> aux;
+		Vector<Vector<Object>> result = new Vector<Vector<Object>>();
+		
+		connection = DriverManager.getConnection(unformatedString);
+		statement = connection.createStatement();
+		result_set = statement.executeQuery(sql);
+		
+		while(result_set.next()) {
+			aux = new Vector<Object>();
+			
+			for(int i = 1; i<= result_set.getMetaData().getColumnCount(); i++) {
+				aux.add(result_set.getObject(i));
+			}
+			result.add(aux);
+		}
+		
+		return result;
+		
 	}
 
 	/**
 	 * The return value is the number of modified rows
 	 * @param sql
+	 * @throws SQLException 
 	 */
-	public int update(String sql) {
-		// TODO - implement Broker.update
-		throw new UnsupportedOperationException();
+	public int update(String sql) throws SQLException {
+		
+		int update = 0;
+		
+		connection = DriverManager.getConnection(unformatedString);
+		PreparedStatement prepared_statement = connection.prepareStatement(sql);
+		update = prepared_statement.executeUpdate();
+		
+		prepared_statement.close();
+		connection.close();
+		
+		return update;
+		
 	}
 
 }
