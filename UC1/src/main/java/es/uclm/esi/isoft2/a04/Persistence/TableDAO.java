0812b421778798734d63a9107acb19f682b6d1ba
Guadalupe-Plaza490 <Guadalupe-Plaza@alu.uclm.es>
2020-12-12 13:44:08 +0100

    New Implementations of the TableDao

--- UC1/src/main/java/es/uclm/esi/isoft2/a04/Persistence/TableDAO.java
+++ UC1/src/main/java/es/uclm/esi/isoft2/a04/Persistence/TableDAO.java
@@ -1,83 +1,81 @@
 package es.uclm.esi.isoft2.a04.Persistence;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.Vector;
 
 import es.uclm.esi.isoft2.a04.Domain.*;
-
+import es.uclm.esi.isoft2.a04.Persistance.Broker;
 
 public class TableDAO {
 
-	public TableImplementation[] readAllTables() {
-		// TODO - implement TableDAO.readAllTables
-		throw new UnsupportedOperationException();
+	public TableImplementation[] readAllTables() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		
+		Vector<Vector<Object>> result = new Vector<Vector<Object>>();
+		TableImplementation[] table_implementation;
+		
+		String sql = "SELECT * FROM Tables"; //Sql sentence
+		
+		return null;
 	}
 
 	/**
 	 * 
 	 * @param table
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
 	 */
-	public void readTable(TableImplementation table) {
+	public void readTable(TableImplementation table) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 		
-		try {
-			String url = "jdbc:mysql://172.20.48.70:3306/XXXdbservice?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-			Connection conn = DriverManager.getConnection(url,"","");
-			Statement statement = conn.createStatement();
-			ResultSet resultQuery;
-			
-			resultQuery = statement.executeQuery("SELECT  FROM Booking WHERE ");
-			
-		}
-		catch (Exception e) {
-			System.err.println("An exception has occur");
-			System.out.println(e.getMessage());
-		}
+		String sql = "SELECT * FROM Tables WHERE TableID ="+table.getID(); //Sql sentence
+		Broker.getBroker().read(sql);
+		
 	}
 
 	/**
 	 * 
 	 * @param table
+	 * @throws ClassNotFoundException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws SQLException 
 	 */
-	public int createTable(TableImplementation table) {
+	public int createTable(TableImplementation table) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 		
-		try {
-			String url = "jdbc:mysql://172.20.48.70:3306/XXXdbservice?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-			Connection conn = DriverManager.getConnection(url,"","");
-			Statement statement = conn.createStatement();
-			ResultSet resultQuery;
-			
-			resultQuery = statement.executeQuery("DELETE FROM  WHERE id = ");
-			
-			return resultQuery.getType();
-			
-		}
-		catch (Exception e) {
-			System.err.println("An exception has occur");
-			System.out.println(e.getMessage());
-		}
-		return -1;
+		String sql = "INSERT INTO Tables VALUES ("+ table.getID()+", "+table.getSeats()+","+table.getSeatsNumber()+","+table.getState()+")"; //Sql sentence
+		return Broker.getBroker().update(sql);
 		
 	}
 		
-
 	/**
 	 * 
 	 * @param table
+	 * @throws ClassNotFoundException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws SQLException 
 	 */
-	public int updateTable(TableImplementation table) {
-		// TODO - implement TableDAO.updateTable
-		throw new UnsupportedOperationException();
+	public int updateTable(TableImplementation table) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		
+		String sql = "UPDATE Tables WHERE TableID ="+table.getID(); //Sql sentence
+		return Broker.getBroker().update(sql);
+		
 	}
 
 	/**
 	 * 
 	 * @param table
+	 * @throws ClassNotFoundException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws SQLException 
 	 */
-	public int deleteOrder(TableImplementation table) {
-		// TODO - implement TableDAO.deleteOrder
-		throw new UnsupportedOperationException();
+	public int deleteOrder(TableImplementation table) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		
+		String sql = "DELETE FROM Tables WHERE TableID ="+table.getID(); //Sql sentence
+		return Broker.getBroker().update(sql);
+		
 	}
 
 }
