bb1574f40cf32f03790af9247168fd98ab608b2a
Maciej Nalepa <maciej.nalepa@alu.uclm.es>
2020-12-16 17:55:54 +0100

    Partially implemented IngredientControl

--- UC3/src/main/java/es/uclm/esi/isoft2/a04/Domain/IngredientControl.java
+++ UC3/src/main/java/es/uclm/esi/isoft2/a04/Domain/IngredientControl.java
@@ -1,33 +1,56 @@
 package es.uclm.esi.isoft2.a04.Domain;
 
-import es.uclm.esi.isoft2.a04.Domain.*;
+import java.util.Arrays;
 
-public class IngredientControl {
+/**
+ * Ingredient Control implementation to allow managing of ingredients in the database
+ * @version 0.0.1
+ */
+public class IngredientControl implements Subject {
 	
 	private Ingredient[] ingredients;
 
 	/**
-	 * 
-	 * @param id
+	 * Get Ingredient at given index
+	 * @param id database array index
 	 */
 	public Ingredient getIngredient(int id) {
-
 		return this.ingredients[id];
-		
-	}
-
-	public Ingredient[] getAllIngredients() {
-		// TODO - implement IngredientControl.getAllIngredients
-		throw new UnsupportedOperationException();
 	}
 
 	/**
-	 * 
-	 * @param ingredient
-	 * @param ammount
+	 * Retrieves ingredients array
+	 * @return array of Ingredient
 	 */
-	public void updateIngredientAmount(Ingredient ingredient, float ammount) {
-		// TODO - implement IngredientControl.updateIngredientAmount
+	public Ingredient[] getAllIngredients() {
+		return ingredients;
+	}
+
+	/**
+	 * Set new amount on existing ingredient
+	 * @param ingredient Ingredient reference to find
+	 * @param amount New amount of desired ingredient
+	 */
+	public void updateIngredientAmount(Ingredient ingredient, float amount) {
+		Ingredient i = Arrays.stream(ingredients).filter(x -> x == ingredient).findFirst().get();
+		i.setAmount(amount);
+	}
+
+	@Override
+	public void attach(Observer o) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void detach(Observer o) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void notifyMe() {
+		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException();
 	}
 
