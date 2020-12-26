a0ba680c953bf98f01aba40b843f3fa34cf21ade
PiotrZdzislawMaliszewski <Piotr.Maliszewski@alu.uclm.es>
2020-12-26 12:28:03 +0100

    UC2 presentation design

--- UC2/src/main/java/es/uclm/esi/isoft2/a04/Presentation/JPanelFood.java
+++ UC2/src/main/java/es/uclm/esi/isoft2/a04/Presentation/JPanelFood.java
@@ -1,9 +1,36 @@
 package es.uclm.esi.isoft2.a04.Presentation;
 
+import javax.swing.JPanel;
+
 import es.uclm.esi.isoft2.a04.Domain.*;
+import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import javax.swing.border.TitledBorder;
 
-public class JPanelFood implements Observer {
+public class JPanelFood extends JPanel implements Observer {
+	
+	private Table table;
+	private Waiter waiter;
+	private JScrollPane scrollPane;
+	private JButton btnReady;
 
+	/**
+	 * Create the panel.
+	 */
+	public JPanelFood(Waiter waiter, Table table) {
+		this.table = table;
+		this.waiter = waiter;
+		setLayout(new BorderLayout(0, 0));
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBorder(new TitledBorder(null, "Order contents", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		add(scrollPane, BorderLayout.CENTER);
+		
+		btnReady = new JButton("Ready");
+		add(btnReady, BorderLayout.EAST);
+	}
+	
 	public void update() {
 		// TODO - implement
 		throw new UnsupportedOperationException();
--- UC2/src/main/java/es/uclm/esi/isoft2/a04/Presentation/JPanelOrder.java
+++ UC2/src/main/java/es/uclm/esi/isoft2/a04/Presentation/JPanelOrder.java
@@ -1,8 +1,50 @@
 package es.uclm.esi.isoft2.a04.Presentation;
 
-import es.uclm.esi.isoft2.a04.Domain.*;
+import javax.swing.JPanel;
 
-public class JPanelOrder implements Observer {
+import es.uclm.esi.isoft2.a04.Domain.*;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import javax.swing.border.TitledBorder;
+
+public class JPanelOrder extends JPanel implements Observer {
+	
+	private Table table;
+	private Waiter waiter;
+	private JButton btnCloseOrder;
+	private JPanel panel;
+	private JButton btnFood;
+	private JButton btnBeverage;
+	private JScrollPane scrollPane;
+	
+	/**
+	 * Create the panel.
+	 */
+	public JPanelOrder(Waiter waiter, Table table) {
+		this.waiter = waiter;
+		this.table = table;
+		setLayout(new BorderLayout(0, 0));
+		
+		btnCloseOrder = new JButton("Close order");
+		add(btnCloseOrder, BorderLayout.SOUTH);
+		
+		panel = new JPanel();
+		add(panel, BorderLayout.NORTH);
+		
+		btnFood = new JButton("Add food");
+		panel.add(btnFood);
+		
+		btnBeverage = new JButton("Add beverage");
+		panel.add(btnBeverage);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBorder(new TitledBorder(null, "Order contents", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		add(scrollPane, BorderLayout.CENTER);
+	}
 
 	public void update() {
 		// TODO - implement
